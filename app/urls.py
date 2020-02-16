c9f0d525680d875cdbec8ca963e0505b185edeec
mukul96 <mukulkathpalia@gmail.com>
2020-02-16 15:35:52 +0530

    all variants added

--- app/urls.py
+++ app/urls.py
@@ -1,32 +1,16 @@
-from .views import UserViewSet,Check
-from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
 from django.urls import path
-from django.urls import include
 
 
-router = DefaultRouter()
-router.register(r'api', UserViewSet, basename='user')
-urlpatterns = router.urls
-app_detail = UserViewSet.as_view({
-    'get': 'retrieveDetail',
-    'put':'updateDetail',
-})
 
-
-# #from test_rest.urls import router
-#
-# for url in router.urls:
-#     print(url.__dict__)
 
 urlpatterns = [
 
 
-    # path('users', UserViewSet.as_view({'get': 'list'}), name='users'),
-    # path('users/<int:pk>', UserViewSet.as_view({'get': 'retrieve'}), name='users'),
-    # path('users', UserViewSet.as_view({'post': 'create'}), name='users'),
-    path('', include(router.urls)),
-    path('retrieveDetail/<int:pk>', app_detail,name='retreive'),
-    path('list',Check.as_view({'get':'list'}),name='list')
+
+    path('users', UserViewSet.as_view({'get':'listUsers','post':'createUser'}),name='listUsers'),
+    path('users/<int:pk>', UserViewSet.as_view({'get':'retrieveDetail','delete':'deleteUser','put':'updateUser'}),name='retreive'),
+
 
 
 ]
--- app/views.py
+++ app/views.py
@@ -6,115 +6,113 @@ from rest_framework.response import Response
 from  .serializers import UserSerializer
 from rest_framework import status
 from django.http import Http404
-from rest_framework.filters import OrderingFilter
-from rest_framework.decorators import action
 from django.db.models import  Q
-from rest_framework.generics import ListAPIView
-# Create your views here.
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.pagination import LimitOffsetPagination
+
+
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size_query_param = 'limit'
+    page_size=5
 
 
 
 
-
-
-class UserViewSet(viewsets.ViewSet):
+class UserViewSet(CustomPageNumberPagination,viewsets.ModelViewSet):
     queryset = UserInfo.objects.all()
-    serializer = UserSerializer
+    serializer_class = UserSerializer
 
-    # def get_paginated_response(self, data):
-    #     return Response({
-    #         'links': {
-    #             'next': self.get_next_link(),
-    #             'previous': self.get_previous_link()
-    #         },
-    #         'count': self.page.paginator.count,
-    #         'page_size': self.page_size,
-    #         'results': data
-    #     })
+
     def get_object(self, pk):
         try:
             return UserInfo.objects.get(pk=pk)
         except UserInfo.DoesNotExist:
             raise Http404("user does not exist")
 
-    @action(detail=False, methods=['get'], url_path='users')
-    def listUsers(self, request, *args, **kwargs):
-        queryset = UserInfo.objects.all()
-        serializer = UserSerializer(queryset, many=True)
-        return Response(serializer.data)
 
-    @action(detail=False, methods=['post'], url_path='users1')
+    def listUsers(self, request):
+        name = request.GET.get('name', None)
+        q_object = Q()
+        if name is not None:
+            q_object |= Q(first_name__icontains=name)
+            q_object |= Q(last_name__icontains=name)
+        userObjects = UserInfo.objects.filter(q_object)
+        order=request.GET.get('sort', None)
+        if order is not None:
+            userObjects = userObjects.order_by(order)
+        #userObjects=userObjects.order_by('first_name')
+        serializedData = UserSerializer(userObjects, many=True)
+        data = self.paginate_queryset(serializedData.data, request)
+
+        return Response(data,status=status.HTTP_200_OK)
+
+
     def createUser(self, request):
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response("user created",status=status.HTTP_201_CREATED)
 
-    @action(detail=False, methods=['get'], url_path='userDetail')
+
     def retrieveDetail(self, request, pk):
         queryset = UserInfo.objects.all()
         user = get_object_or_404(queryset, pk=pk)
         serializer = UserSerializer(user)
-        return Response(serializer.data)
-
-
-    @action(detail=False,methods=['get'],url_path='users1')
-    def userDetail(self,request):
-
-        name=request.GET.get('name')
-        user=UserInfo.objects.filter(Q(first_name__icontains=name) | Q(last_name__icontains=name))
-        serializer=UserSerializer(user,many=True)
-
         return Response(serializer.data,status=status.HTTP_200_OK)
 
+    def updateUser(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = UserSerializer(snippet, data=request.data,partial=True)
+        if serializer.is_valid():
+            self.perform_update(serializer)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
+    def deleteUser(self, request, pk):
         user = self.get_object(pk)
         user.delete()
         return Response("deleted",status=status.HTTP_200_OK)
 
 
 
-class Check(viewsets.ModelViewSet):
-    queryset = UserInfo.objects.all()
-    serializer_class = UserSerializer
-    #pagination_class = PageNumberPagination
 
-    def list(self, request):
-        serializer = self.get_serializer_class()
-        paginator = LimitOffsetPagination()
-        paginator.default_limit = 15
-        paginator.limit = paginator.get_limit(request)
-        paginator.offset = paginator.get_offset(request)
-        feeds = paginator.paginate_queryset(UserInfo.objects.all(), request)
-        return Response(
-            data={
-                'feeds': serializer(feeds, many=True).data,
-                'limit': paginator.limit,
-                'offset': paginator.offset,
-                'overall_count': paginator.count
-            }
-        )
-        # data = [
-        #     {"id": 1},
-        #     {"id": 2},
-        # ]
-        # data=self.get_queryset()
-        # #data=UserSerializer(data,many=True)
-        # #print(data)
-        # paginator = PageNumberPagination()
-        # page = paginator.paginate_queryset(data, request)
-        # print(page)
-        # print("checking the work")
-        # page=UserSerializer(page,many=True)
-        # #print*
-        # #print*
-        # if page is not None:
-        #     return paginator.get_paginated_response(page)
-        #
-        # return Response(data)
+
+
+
+
+
+
+
+
+
+
+
+
+# class CustomPageNumberPagination(PageNumberPagination):
+#     page_size_query_param = 'limit'
+#
+# class Check(viewsets.ModelViewSet):
+#     queryset = UserInfo.objects.all()
+#     serializer_class = UserSerializer
+#     pagination_class = CustomPageNumberPagination
+#
+#
+#
+#     def list(self, request):
+#
+#
+#
+#         name = request.GET.get('name',None)
+#         q_object=Q()
+#         if name is not None:
+#             q_object |= Q(first_name__icontains=name)
+#             q_object |= Q(last_name__icontains=name)
+#         userObjects = UserInfo.objects.filter(q_object)
+#         serializedData=UserSerializer(userObjects,many=True)
+#         paginator = CustomPageNumberPagination()
+#         data = paginator.paginate_queryset(serializedData.data, request)
+#
+#         return Response(data)
+
 
 
 
